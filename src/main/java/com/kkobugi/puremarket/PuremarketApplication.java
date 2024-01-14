6ce83098af1e8ec38bb8eef6cea634b7e896813b
joonghyun <joongh@ewhain.net>
2024-01-14 17:15:15 +0900

    Initialize the project

---
+++ src/main/java/com/kkobugi/puremarket/PuremarketApplication.java
@@ -0,0 +1,13 @@
+package com.kkobugi.puremarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuremarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuremarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkobugi/puremarket/PuremarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkobugi.puremarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuremarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
