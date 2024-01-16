0733fcac4e003cfcd077ccc20578dc76150f3b1e
joonghyun <joongh@ewhain.net>
2024-01-16 21:17:50 +0900

    #2 feat: SwaggerConfig 파일 추가

--- src/main/java/com/kkobugi/puremarket/common/config/SwaggerConfig.java
+++ src/main/java/com/kkobugi/puremarket/common/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Pure Market API", description = "2024 Google Soulution Challenge Pure Market Project", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Pure Market API", description = "2024 Google Solution Challenge Pure Market Project", version = "v1"))
 public class SwaggerConfig {
 
     @Bean
