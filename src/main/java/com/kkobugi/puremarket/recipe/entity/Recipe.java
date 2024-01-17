04d060dd59b66ed4da14d0cbc1be5f87ae50eca9
joonghyun <joongh@ewhain.net>
2024-01-17 17:27:03 +0900

    #1 fix: Recipe Entity에 User와의 연관관계 추가

--- src/main/java/com/kkobugi/puremarket/recipe/entity/Recipe.java
+++ src/main/java/com/kkobugi/puremarket/recipe/entity/Recipe.java
@@ -1,6 +1,7 @@
 package com.kkobugi.puremarket.recipe.entity;
 
 import com.kkobugi.puremarket.common.BaseEntity;
+import com.kkobugi.puremarket.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,10 @@ public class Recipe extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long recipeIdx;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "userIdx")
+    private User user;
 
     @Column(nullable = false, length = 100)
     private String title;
