00b4c18bc2637b441427490c6fcd809453931f7a
joonghyun <joongh@ewhain.net>
2024-01-17 17:27:24 +0900

    #1 fix: Produce Entity에 User와의 연관관계 추가

--- src/main/java/com/kkobugi/puremarket/produce/entity/Produce.java
+++ src/main/java/com/kkobugi/puremarket/produce/entity/Produce.java
@@ -1,6 +1,7 @@
 package com.kkobugi.puremarket.produce.entity;
 
 import com.kkobugi.puremarket.common.BaseEntity;
+import com.kkobugi.puremarket.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,10 @@ public class Produce extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long produceIdx;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "userIdx")
+    private User user;
 
     @Column(nullable = false, length = 100)
     private String title;
