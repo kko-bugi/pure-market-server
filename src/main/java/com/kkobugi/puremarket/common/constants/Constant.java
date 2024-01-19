9029113b4eb75572ca1da327e97b65e001954c35
joonghyun <joongh@ewhain.net>
2024-01-19 19:17:55 +0900

    #5 chore: constant 파일 생성

---
+++ src/main/java/com/kkobugi/puremarket/common/constants/Constant.java
@@ -0,0 +1,6 @@
+package com.kkobugi.puremarket.common.constants;
+
+public class Constant {
+    public static final String ACTIVE = "active";
+    public static final String INACTIVE = "inactive";
+}
