eac86d87d7c44cf83f99e7b5de404e34fc7840db
joonghyun <joongh@ewhain.net>
2024-01-19 19:20:14 +0900

    #5 chore: RequestURI 상수 파일 생성

---
+++ src/main/java/com/kkobugi/puremarket/common/constants/RequestURI.java
@@ -0,0 +1,6 @@
+package com.kkobugi.puremarket.common.constants;
+
+public class RequestURI {
+    private final static String baseURI = "/api/v1";
+    public final static String user = baseURI + "/users";
+}
