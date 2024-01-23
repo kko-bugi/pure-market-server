5032f0925d3b394e26edcc344a7bc06a2b0b6d22
joonghyun <joongh@ewhain.net>
2024-01-23 19:10:40 +0900

    #5 fix: 불필요한 annotation 제거 및 빈 주입 방법 변경

--- src/main/java/com/kkobugi/puremarket/common/configuration/WebSecurityConfig.java
+++ src/main/java/com/kkobugi/puremarket/common/configuration/WebSecurityConfig.java
@@ -1,8 +1,9 @@
-package com.kkobugi.puremarket.user.configuration;
+package com.kkobugi.puremarket.common.configuration;
 
+import com.kkobugi.puremarket.user.application.AuthService;
+import com.kkobugi.puremarket.user.application.UserService;
 import com.kkobugi.puremarket.user.utils.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,23 +12,29 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-    private final JwtTokenFilter jwtTokenFilter;
-    @Value("${jwt.secret-key}")
+    private final UserService userService;
+    private final AuthService authService;
 
+//    @Value("${jwt.secret-key}")
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((sessionManagement) ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests.anyRequest().permitAll()) //TODO: 마이페이지 접근 권한 설정 추가
-                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                        .requestMatchers( // TODO: 접근 권한 수정 필요
+                                new AntPathRequestMatcher("/"),
+                                new AntPathRequestMatcher("/api/v1/**")).permitAll()
+                        .anyRequest().authenticated()
+                ) //TODO: 마이페이지 접근 권한 설정 추가
+                .addFilterBefore(new JwtTokenFilter(authService, userService), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
