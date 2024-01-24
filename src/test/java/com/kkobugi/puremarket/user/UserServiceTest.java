a5a1e96d45b81fc30774420dad897ed6da440694
joonghyun <joongh@ewhain.net>
2024-01-24 16:03:36 +0900

    #5 feat: 로그인 로직 테스트

--- src/test/java/com/kkobugi/puremarket/user/UserServiceTest.java
+++ src/test/java/com/kkobugi/puremarket/user/UserServiceTest.java
@@ -2,6 +2,8 @@ package com.kkobugi.puremarket.user;
 
 import com.kkobugi.puremarket.common.BaseException;
 import com.kkobugi.puremarket.user.application.UserService;
+import com.kkobugi.puremarket.user.domain.dto.LoginRequest;
+import com.kkobugi.puremarket.user.domain.dto.LoginResponse;
 import com.kkobugi.puremarket.user.domain.dto.SignupRequest;
 import com.kkobugi.puremarket.user.domain.entity.User;
 import com.kkobugi.puremarket.user.repository.UserRepository;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static com.kkobugi.puremarket.common.enums.BaseResponseStatus.INVALID_LOGIN_ID;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -33,9 +36,25 @@ class UserServiceTest {
         userService.signup(signupRequest);
 
         // then
-        User user = userRepository.findByLoginId("id").orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepository.findByLoginId("id").orElseThrow(() -> new BaseException(INVALID_LOGIN_ID));
         assertEquals("nickname", user.getNickname());
         assertTrue(encoder.matches("pw", user.getPassword()));
         assertEquals("01012345678", user.getContact());
+    }
+
+    @Test
+    @DisplayName("로그인")
+    public void login() throws BaseException {
+        // given
+        LoginRequest loginRequest = new LoginRequest("id", "pw");
+
+        // when
+        LoginResponse loginResponse = userService.login(loginRequest);
+
+        // then
+        User user = userRepository.findByLoginId("id").orElseThrow(() -> new BaseException(INVALID_LOGIN_ID));
+        assertNotNull(loginResponse.accessToken());
+        assertTrue(encoder.matches("pw", user.getPassword()));
+        assertEquals("id", user.getLoginId());
     }
 }
