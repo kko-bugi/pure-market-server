c0b303730ccbba56d8d3a16503578c7206e9f61e
joonghyun <joongh@ewhain.net>
2024-02-02 21:59:34 +0900

    #8 feat: 판매글 목록 조회 API

---
+++ src/main/java/com/kkobugi/puremarket/produce/domain/dto/ProduceListResponse.java
@@ -0,0 +1,12 @@
+package com.kkobugi.puremarket.produce.domain.dto;
+
+import java.util.List;
+
+public record ProduceListResponse(List<ProduceDto> produceList) {
+    public record ProduceDto(
+            Long produceIdx,
+            String title,
+            Integer price,
+            String produceImage) {
+    }
+}
