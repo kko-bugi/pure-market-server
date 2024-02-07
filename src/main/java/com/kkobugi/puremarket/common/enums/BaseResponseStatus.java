5fe58e0ad5274f24058d3b430397afdee2bbfdcb
joonghyun <joongh@ewhain.net>
2024-02-07 15:51:02 +0900

    #1 chore: BaseResponseStatus 순서 변경

--- src/main/java/com/kkobugi/puremarket/common/enums/BaseResponseStatus.java
+++ src/main/java/com/kkobugi/puremarket/common/enums/BaseResponseStatus.java
@@ -13,14 +13,15 @@ public enum BaseResponseStatus {
      * 2000: Request 오류
      */
     // users(2000-2099)
-    DUPLICATED_LOGIN_ID(false, 2000, "이미 사용중인 아이디입니다."),
-    DUPLICATED_NICKNAME(false, 2001, "이미 사용중인 닉네임입니다."),
-    UNMATCHED_PASSWORD(false, 2002, "비밀번호가 일치하지 않습니다."),
-    INVALID_PASSWORD(false, 2003, "비밀번호가 잘못되었습니다."),
-    INVALID_LOGIN_ID(false, 2004, "잘못된 아이디입니다."),
-    INVALID_ACCESS_TOKEN(false, 2005, "잘못된 AccessToken 입니다."),
-    INVALID_REFRESH_TOKEN(false, 2006, "잘못된 Refresh Token 입니다."),
-    NULL_ACCESS_TOKEN(false, 2007, "Access Token을 입력해주세요."),
+    INVALID_USER_IDX(false, 2000, "user idx가 잘못되었습니다."),
+    DUPLICATED_LOGIN_ID(false, 2001, "이미 사용중인 아이디입니다."),
+    DUPLICATED_NICKNAME(false, 2002, "이미 사용중인 닉네임입니다."),
+    UNMATCHED_PASSWORD(false, 2003, "비밀번호가 일치하지 않습니다."),
+    INVALID_PASSWORD(false, 2004, "비밀번호가 잘못되었습니다."),
+    INVALID_LOGIN_ID(false, 2005, "잘못된 아이디입니다."),
+    INVALID_ACCESS_TOKEN(false, 2006, "잘못된 AccessToken 입니다."),
+    INVALID_REFRESH_TOKEN(false, 2007, "잘못된 Refresh Token 입니다."),
+    NULL_ACCESS_TOKEN(false, 2008, "Access Token을 입력해주세요."),
 
     // produce(2100-2199)
     INVALID_PRODUCE_IDX(false, 2100, "잘못된 판매글 idx 입니다."),
@@ -45,8 +46,7 @@ public enum BaseResponseStatus {
      * 3000: Response 오류
      */
     // users(3000~3099)
-    INVALID_USER_IDX(false, 3000, "user idx가 잘못 되었습니다."),
-    NO_MATCH_USER(false, 3001, "해당 user를 찾을 수 없습니다."),
+    NO_MATCH_USER(false, 3000, "해당 user를 찾을 수 없습니다."),
 
     // produce(3100-3199)
     NULL_PRODUCE_LIST(false, 3100, "판매글 목록이 비었습니다."),
