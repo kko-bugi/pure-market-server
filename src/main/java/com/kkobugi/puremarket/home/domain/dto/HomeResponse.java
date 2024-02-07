9dc8294494f9fb5515401fada0c07bbb7a2e5e44
joonghyun <joongh@ewhain.net>
2024-02-07 16:48:22 +0900

    #2 feat: home API 상세 설명 및 예시 추가

--- src/main/java/com/kkobugi/puremarket/home/domain/dto/HomeResponse.java
+++ src/main/java/com/kkobugi/puremarket/home/domain/dto/HomeResponse.java
@@ -1,21 +1,42 @@
 package com.kkobugi.puremarket.home.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
-public record HomeResponse(List<ProduceDto> produceList,
-                           List<RecipeDto> recipeList,
-                           List<GiveawayDto> giveawayList) {
-    public record ProduceDto(Long produceIdx,
-                             String title,
-                             Integer price,
-                             String produceImage){}
+@Schema(description = "홈화면 응답")
+public record HomeResponse(
+        @Schema(description = "최근 판매글 리스트")
+        List<ProduceDto> produceList,
+        @Schema(description = "최근 레시피글 리스트")
+        List<RecipeDto> recipeList,
+        @Schema(description = "최근 나눔글 리스트")
+        List<GiveawayDto> giveawayList) {
+    public record ProduceDto(
+            @Schema(description = "판매글 Idx", example = "1")
+            Long produceIdx,
+            @Schema(description = "글 제목", example = "판매글")
+            String title,
+            @Schema(description = "가격", example = "10000")
+            Integer price,
+            @Schema(description = "글 이미지 url", example = "https://dwffwdfdwbwv")
+            String produceImage){}
 
-    public record RecipeDto(Long recipeIdx,
-                            String title,
-                            String recipeImage){}
+    public record RecipeDto(
+            @Schema(description = "레시피글 Idx", example = "1")
+            Long recipeIdx,
+            @Schema(description = "글 제목", example = "레시피글")
+            String title,
+            @Schema(description = "글 이미지 url", example = "https://dwffwdfdwbwv")
+            String recipeImage){}
 
-    public record GiveawayDto(Long giveawayIdx,
-                              String title,
-                              String content,
-                              String giveawayImage){}
+    public record GiveawayDto(
+            @Schema(description = "나눔 Idx", example = "1")
+            Long giveawayIdx,
+            @Schema(description = "글 제목", example = "나눔글")
+            String title,
+            @Schema(description = "글 내용", example = "나눔합니다")
+            String content,
+            @Schema(description = "글 이미지 url", example = "https://dwffwdfdwbwv")
+            String giveawayImage){}
 }
--- src/main/java/com/kkobugi/puremarket/home/presentation/HomeController.java
+++ src/main/java/com/kkobugi/puremarket/home/presentation/HomeController.java
@@ -3,6 +3,10 @@ package com.kkobugi.puremarket.home.presentation;
 import com.kkobugi.puremarket.common.BaseException;
 import com.kkobugi.puremarket.common.BaseResponse;
 import com.kkobugi.puremarket.home.application.HomeService;
+import com.kkobugi.puremarket.home.domain.dto.HomeResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +26,9 @@ public class HomeController {
     private final HomeService homeService;
 
     @GetMapping("")
-    public BaseResponse<?> getHome() {
+    @Operation(summary = "홈화면 조회", description = "홈화면을 조회한다. 글 목록이 비었을 시 예외처리 없이 빈 리스트를 반환한다.")
+    @ApiResponses(value = {@ApiResponse(responseCode = "1000", description = "조회 성공")})
+    public BaseResponse<HomeResponse> getHome() {
         try {
             return new BaseResponse<>(homeService.getHome());
         } catch (BaseException e) {
