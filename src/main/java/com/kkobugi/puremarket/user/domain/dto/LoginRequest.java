7d359a1359edaf5b2170d12f14f758c9d0199e08
joonghyun <joongh@ewhain.net>
2024-02-07 16:36:10 +0900

    #2 feat: users API request, response 설명 및 예시 추가

--- src/main/java/com/kkobugi/puremarket/user/domain/dto/LoginRequest.java
+++ src/main/java/com/kkobugi/puremarket/user/domain/dto/LoginRequest.java
@@ -1,5 +1,10 @@
 package com.kkobugi.puremarket.user.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "로그인 요청")
 public record LoginRequest(
+        @Schema(description = "로그인 아이디", example = "id")
         String loginId,
+        @Schema(description = "비밀번호", example = "pwpwpw")
         String password) {}
--- src/main/java/com/kkobugi/puremarket/user/domain/dto/SignupRequest.java
+++ src/main/java/com/kkobugi/puremarket/user/domain/dto/SignupRequest.java
@@ -1,14 +1,21 @@
 package com.kkobugi.puremarket.user.domain.dto;
 
 import com.kkobugi.puremarket.user.domain.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "회원가입 요청")
 public record SignupRequest(
+        @Schema(description = "닉네임", example = "꼬부기")
         String nickname,
+        @Schema(description = "로그인 아이디", example = "id")
         String loginId,
+        @Schema(description = "비밀번호", example = "pwpwpw")
         String password,
+        @Schema(description = "비밀번호 확인", example = "pwpwpw")
         String passwordCheck,
+        @Schema(description = "연락처", example = "01012345678")
         String contact) {
 
     public User toUser(String encodedPassword, String imageUrl) {
