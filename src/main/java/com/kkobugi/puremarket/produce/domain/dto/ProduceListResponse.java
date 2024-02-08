b5073467310f9238ff56654a152fef26968791cf
joonghyun <joongh@ewhain.net>
2024-02-08 16:28:58 +0900

    #2 feat: produce API 상세 설명 및 예시 추가

--- src/main/java/com/kkobugi/puremarket/produce/domain/dto/ProduceListResponse.java
+++ src/main/java/com/kkobugi/puremarket/produce/domain/dto/ProduceListResponse.java
@@ -1,13 +1,22 @@
 package com.kkobugi.puremarket.produce.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
-public record ProduceListResponse(List<ProduceDto> produceList) {
+@Schema(description = "판매글 목록 응답")
+public record ProduceListResponse(
+        @Schema(description = "판매글 리스트")
+        List<ProduceDto> produceList) {
     public record ProduceDto(
+            @Schema(description = "판매글 Idx", example = "1")
             Long produceIdx,
+            @Schema(description = "글 제목", example = "판매글")
             String title,
+            @Schema(description = "가격", example = "10000")
             Integer price,
+            @Schema(description = "글 이미지 url", example = "https://dwffwdfdwbwv")
             String produceImage,
-            String produceStatus) {
-    }
+            @Schema(description = "판매 상태", example = "판매완료")
+            String produceStatus) {}
 }
--- src/main/java/com/kkobugi/puremarket/produce/domain/dto/ProduceResponse.java
+++ src/main/java/com/kkobugi/puremarket/produce/domain/dto/ProduceResponse.java
@@ -1,12 +1,26 @@
 package com.kkobugi.puremarket.produce.domain.dto;
 
-public record ProduceResponse(Long produceIdx,
-                              String title,
-                              String content,
-                              Integer price,
-                              String produceImage,
-                              String produceStatus,
-                              String nickname,
-                              String contact,
-                              String profileImage,
-                              boolean isWriter) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "판매글 상세 조회 응답")
+public record ProduceResponse(
+        @Schema(description = "판매글 Idx", example = "1")
+        Long produceIdx,
+        @Schema(description = "글 제목", example = "판매글")
+        String title,
+        @Schema(description = "글 내용", example = "판매함니다~")
+        String content,
+        @Schema(description = "가격", example = "10000")
+        Integer price,
+        @Schema(description = "글 이미지 url", example = "https://dwffwdfdwbwv")
+        String produceImage,
+        @Schema(description = "핀매 상태", example = "판매완료")
+        String produceStatus,
+        @Schema(description = "닉네임", example = "꼬부기")
+        String nickname,
+        @Schema(description = "연락처", example = "01012345678")
+        String contact,
+        @Schema(description = "글 이미지 url", example = "https://dwffwdfdwbwv")
+        String profileImage,
+        @Schema(description = "글 작성자 여부", example = "true")
+        boolean isWriter) {}
