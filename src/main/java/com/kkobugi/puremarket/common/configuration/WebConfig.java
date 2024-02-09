dfa21158379a56ca6b112b8f9938c7ff33d050b1
joonghyun <joongh@ewhain.net>
2024-02-10 01:19:34 +0900

    #62 fix: allowCredentials false 설정

--- src/main/java/com/kkobugi/puremarket/common/configuration/WebConfig.java
+++ src/main/java/com/kkobugi/puremarket/common/configuration/WebConfig.java
@@ -5,15 +5,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
 
