563285be4894ebe8f1528b547c1c08b6727d4852
joonghyun <joongh@ewhain.net>
2024-02-23 21:47:20 +0900

    #1 feat: Comment entity 추가

---
+++ src/main/java/com/kkobugi/puremarket/comment/domain/entity/Comment.java
@@ -0,0 +1,42 @@
+package com.kkobugi.puremarket.comment.domain.entity;
+
+import com.kkobugi.puremarket.common.BaseEntity;
+import com.kkobugi.puremarket.giveaway.domain.entity.Giveaway;
+import com.kkobugi.puremarket.produce.domain.entity.Produce;
+import com.kkobugi.puremarket.recipe.domain.entity.Recipe;
+import com.kkobugi.puremarket.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentIdx;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "userIdx")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe")
+    private Recipe recipe;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produce")
+    private Produce produce;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "giveaway")
+    private Giveaway giveaway;
+
+    @Column(nullable = false)
+    private String content;
+}
